e8775b1dcfb0779450905635ab22bcbe7c533c06
Krris <krrisgit@gmail.com>
2014-03-15 21:08:03 +0100

    Add Range class and unit tests for it.

---
+++ src/main/java/io/github/krris/qlearning/Range.java
@@ -0,0 +1,68 @@
+package io.github.krris.qlearning;
+
+public class Range {
+	public final int min;
+	public final int max;
+	
+	private double value;
+	
+	public Range(int min, int max) {
+		this.min = min;
+		this.max = max;
+	}
+	
+	public Range(int min, int max, double value) {
+		this.min = min;
+		this.max = max;
+		this.value = value;
+	}
+	
+	public Range(Range other) {
+		this.min = other.min;
+		this.max = other.max;
+	}
+	
+	public Range(Range other, double value) {
+		this.min = other.min;
+		this.max = other.max;
+		this.value = value;
+	}
+	
+	public boolean fits(double value) {
+		if (value >= min && value < max)
+			return true;
+		return false;
+	}
+
+    /**
+     * Range is equal when min and max value of another Range object are the same.
+     * Value is not considered.
+     * @param other
+     * @return
+     */
+    @Override
+    public boolean equals(Object other) {
+        if (other == null)
+            return false;
+        if (other == this)
+            return true;
+        if (other instanceof Range) {
+            return (((Range)other).min == min &&
+            		((Range)other).max == max);
+        }
+        return false;
+    }
+	
+	@Override
+	public String toString() {
+		return "Range: [" + min + ", " + max + "]";
+	}
+
+	public double getValue() {
+		return value;
+	}
+
+	public void setValue(double value) {
+		this.value = value;
+	}
+}
---
+++ src/test/java/RangeTest.java
@@ -0,0 +1,50 @@
+import io.github.krris.qlearning.Range;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by krris on 15.03.14.
+ */
+@RunWith(JUnit4.class)
+public class RangeTest {
+
+    @Test
+    public void equals() {
+        int min = 100;
+        int max = 200;
+        Range range = new Range(min, max);
+
+        // For any non-null reference value x, x.equals(null) must return false.
+        Range other = null;
+        assertFalse(range.equals(other));
+
+        // Reflexive: For any non-null reference value x, x.equals(x) must return true
+        assertTrue(range.equals(range));
+
+        // Symmetric: For any non-null reference values x and y, x.equals(y) must
+        // return true if and only if y.equals(x) returns true.
+        other = new Range(min, max);
+        assertTrue(range.equals(other) == other.equals(range));
+
+        // Transitive: For any non-null reference values x, y, z, if x.equals(y) returns
+        // true and y.equals(z) returns true, then x.equals(z) must return true.
+        Range other2 = new Range(min, max);
+        assertTrue(range.equals(other));
+        assertTrue(other.equals(other2));
+        assertTrue(range.equals(other2));
+
+        // Consistent: For any non-null reference values x and y, multiple invocations
+        // of x.equals(y) consistently return true or consistently return false, provided
+        // no information used in equals comparisons on the objects is modified.
+        int limit = 1000;
+        Range different = new Range(max, min);
+        for (int i = 0; i < limit ; i++) {
+            assertTrue(range.equals(other));
+            assertFalse(range.equals(different));
+        }
+    }
+}
