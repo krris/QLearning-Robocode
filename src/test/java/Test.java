e1bb3f0a2efd301b84117eda0ea23979ca244b61
Krris <krrisgit@gmail.com>
2014-03-15 18:27:53 +0100

    Add slf4j.

---
+++ src/test/java/Test.java
@@ -0,0 +1,12 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by krris on 15.03.14.
+ */
+public class Test {
+    private static Logger logger = LoggerFactory.getLogger(Test.class);
+    public static void main(String[] args) {
+        logger.info("Info from logger");
+    }
+}
