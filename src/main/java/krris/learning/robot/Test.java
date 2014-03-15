bfbbd2a562e7b3777e4958de1950ae5f1dd71f1f
Krris <krrisgit@gmail.com>
2014-03-15 16:45:35 +0100

    Add libs.

---
+++ src/main/java/krris/learning/robot/Test.java
@@ -0,0 +1,10 @@
+package krris.learning.robot;
+
+/**
+ * Created by krris on 15.03.14.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Bla");
+    }
+}
