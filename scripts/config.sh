bd61989070f229a80b9b41cf7c92c83da2f9a4fc
Krris <krrisgit@gmail.com>
2014-06-09 16:11:52 +0200

    New README. Fix some paths in config.sh.

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 # Configuration file
 
-# Project root folder
-projectSourcePath='/home/krris/programowanie/idea-robot/QLearning-Robocode'
-# Place where the compiled project is located
-compiledProjectPath='/home/krris/programowanie/idea-robot/QLearning-Robocode/out'
 # Direcotry where Robocode is installed
 robocodePath='/home/krris/robo/robocode-1.9.2.1'
+
+pwd=`pwd`
+cd ..
+rootFolder=`pwd`
+cd ${pwd}
+
+# Project root folder
+projectSourcePath="${rootFolder}"
+# Place where the compiled project is located
+compiledProjectPath="${rootFolder}/out"
