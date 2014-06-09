c3aa40c90ca77ac96e6e4c93dfcce17256441ab1
Krris <krrisgit@gmail.com>
2014-06-09 10:52:52 +0200

    Can modify configuration without project compilation.

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,6 +3,9 @@
 
 pwd=`pwd`
 
+./battleConfigurator.sh
+cd ..
+
 robocode=~/robo/robocode-1.9.2.1/
 results=log/results.txt
 battle=battles/generated.battle
@@ -11,9 +14,7 @@ log4jProperties=src/main/java/io/github/krris/qlearning/log4j.properties
 args="-nodisplay -results $results -battle $battle"
 jvmArgs="-Xmx1024m -Ddebug=true -DNOSECURITY=true -DWORKINGDIRECTORY=$robocode -Dlog4j.configuration=file:$log4jProperties"
 
-./battleConfigurator.sh
-cd ..
-java $jvmArgs -cp "libs/robocode.jar:libdeps/*" robocode.Robocode $*  $args
+java $jvmArgs -cp "libs/robocode.jar:libdeps/*" robocode.Robocode $* $args
 cd /home/krris/programowanie/idea-robot/out/production/QLearning-Robocode
 cd "${pwd}"
-./print.sh
+./printChart.sh
